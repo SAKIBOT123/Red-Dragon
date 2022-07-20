95a35b9d8e87a660a64f680ac8f44ba94cdba951
SAKIBOT123 <109673850+SAKIBOT123@users.noreply.github.com>
2022-07-20 21:06:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is 𝚂𝙰𝙺𝙸 𝙱𝙾𝚃🔥
+My Creator is 𝚂𝙰𝙺𝙸` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94705000112'] //Owner number in aive msg
+global.premium = ['+94705000112'] //Owner Number info
+global.ownernomer = '+94705000112' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎Saki' //Owner Name
+global.botname = ' ☬SAKI༒BOT࿐' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ TIKTOK ☰' //Costomize A Button Name In Alive Message
+global.btnurl = ' https://vm.tiktok.com/ZSddMePQN/ ' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬SAKI༒BOT࿐' //Bot Pacage Name
+global.author = '😎SAKI' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_SAKI.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://vm.tiktok.com/ZSddMePQN/' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
